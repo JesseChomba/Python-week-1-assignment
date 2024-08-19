b07d4c9de0b175bb7168f1b4b0c860a0e98a84d9
Jesse Chomba <jessechomba2@gmail.com>
2024-08-19 13:26:10 +0300

    Answers to Python-week-1-assignment

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=str(input(("What is your name?")))
+age=int(input(("What is your age?")))
+location=str(input(("What is your location?")))
+print(f"Hello {name}, you are {age} years old and live in {location}")
